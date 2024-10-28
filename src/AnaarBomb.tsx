2a5ed06342a239982690b378a681edbf6aad0529
Anurag Khugshal <113304552+Anuragk7@users.noreply.github.com>
2024-10-28 13:00:40 +0530

    Update AnaarBomb.tsx

--- src/AnaarBomb.tsx
+++ src/AnaarBomb.tsx
@@ -10,7 +10,7 @@ const Cracker: React.FC<RectangleAreaProps> = ({ isBurning }) => {
     <div
       id="cracker"
       // className="absolute left-1/2 transform -translate-x-1/2 bottom-0 rotate-90" 
-      className={`w-48 h-32  bg-black absolute bottom-0 left-1/2 transform -translate-x-1/2`}
+      className={`w-48 h-32  bg-black absolute top-3/4 left-1/2 transform -translate-x-1/2`}
       style={{
         backgroundImage: "firecracker-9079453_1280.png", // Replace with your image URL
       }}
